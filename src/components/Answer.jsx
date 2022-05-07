f80b2c359889944c6de697f75ee2be2de1710d58
DGouron <damien.gouron@gmail.com>
2022-05-07 11:54:13 +0200

    debug

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -15,10 +15,13 @@ const StyledAnswer = styled.div`
   margin: 10px;
 `;
 
-const defaultTheme = { outline: '1px solid #650a0b' };
-const rightTheme = { outline: '5px solid green' };
-const wrongTheme = { outline: '2px solid red' };
 const hoverOutlineColor = '2px solid #909595';
+const defaultTheme = {
+  outline: '1px solid #650a0b',
+  hoverOutline: hoverOutlineColor,
+};
+const rightTheme = { outline: '5px solid green', hoverOutline: '' };
+const wrongTheme = { outline: '2px solid red', hoverOutline: '' };
 
 const StyledButton = styled.button`
   transition: 0.1s outline ease;
@@ -41,8 +44,11 @@ const StyledButton = styled.button`
   min-width: 128px;
   height: 96px;
 
-  &:hover {
-    outline: ${hoverOutlineColor};
+  &:active {
+    outline: ${(props) => props.theme.outline || '1px solid #650a0b'};
+  }
+  &:visited {
+    outline: ${(props) => props.theme.outline || '1px solid #650a0b'};
   }
 
   @media (max-width: 1080px) {
@@ -55,6 +61,7 @@ const StyledButton = styled.button`
 
 function Answer(props) {
   const [idOfthisAnswer] = useState(props.idOfthisAnswer);
+  const [activeButton, setActiveButton] = useState(false);
   const answerTraitment = useSelector(currentAnswerTraitmentSelector);
   const goodAnswersIds = useSelector(currentGoodAnswersSelector);
 
@@ -65,12 +72,14 @@ function Answer(props) {
 
   useEffect(() => {
     if (answerTraitment) {
+      setActiveButton(true);
       goodAnswersIds.includes(idOfthisAnswer)
         ? setThemeName(rightTheme)
         : setThemeName(wrongTheme);
       updateRender();
     }
     if (answerTraitment === false) {
+      setActiveButton(false);
       setThemeName(defaultTheme);
     }
   }, [answerTraitment, idOfthisAnswer, goodAnswersIds, updateRender]);
@@ -90,7 +99,7 @@ function Answer(props) {
             dispatch(updateSelectedAnswerId(idOfthisAnswer));
             selectThisAnswer();
           }}
-          disabled={props.answered ? 'disabled' : ''}
+          disabled={activeButton}
         >
           {props.content}
         </StyledButton>
